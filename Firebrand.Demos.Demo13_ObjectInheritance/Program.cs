d6a8b9369e490c9e267fcfa757f3edc527ff7fe8
jipson <jipson.thomas@gmail.com>
2016-09-27 06:20:02 -0700

    sealed demo

--- Firebrand.Demos.Demo13_ObjectInheritance/Program.cs
+++ Firebrand.Demos.Demo13_ObjectInheritance/Program.cs
@@ -12,10 +12,40 @@ namespace Firebrand.Demos
         {
             Dog rober = new Dog("This dog can say hello");
             rober.Vocalize();
+            GermenShepherd max = new GermenShepherd();
+            max.Vocalize();
+            Monkey KingLouise = new Monkey();
+            KingLouise.Vocalize();
+
+            var animals = new IAnimal[]{
+                new Dog(),
+                new GermenShepherd(),
+                new Monkey()};
+            foreach (var animal in animals)
+            {
+                animal.Vocalize();
+            }
+            Dog Spock = new GermenShepherd();
+            GermenShepherd Harry = new GermenShepherd();
+            Spock.DisplayName();
+            Harry.DisplayName();
             Console.ReadKey();
         }
         
        
+    }
+    public class Monkey : Animal
+    { 
+       public Monkey() : base(2,"Ooooh monkey sound!!!")
+        {
+        }
+        // Overriding base class method
+        public override void DisplayName()
+        {
+             // To extend from parent class
+            //base.DispayName();
+            Console.WriteLine("Monkey");
+        }
     }
     public class Dog : Animal
     {
@@ -34,6 +64,18 @@ namespace Firebrand.Demos
              // To extend from parent class
             //base.DispayName();
             Console.WriteLine("Dog");
+        }
+    }
+    public class GermenShepherd : Dog
+    {
+        public GermenShepherd()
+            : base("Big woolf from GS")
+        {
+ 
+        }
+        public new void DisplayName()
+        {
+            Console.WriteLine("German Shepherd");
         }
     }
 }
---
+++ Firebrand.Demos.Demo14_EextensionMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firebrand.Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Email Address");
+            string email = Console.ReadLine();
+            Console.WriteLine("Is {0} a valid email address ? {1}", email, email.IsNormalized());
+            Console.ReadKey();
+        }
+    }
+
+    static class MyFunctions
+    {
+        public static bool IsValidEmail(this string input)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(input, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+        }
+        public static bool IsNormalized(this string input)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(input, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+        }
+        
+    }
+}
