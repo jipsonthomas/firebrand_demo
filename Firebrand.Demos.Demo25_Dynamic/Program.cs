4165e118c2324e65ae1cb80b020e30f685dd37ee
jipson <jipson.thomas@gmail.com>
2016-09-28 09:49:43 -0700

    Unmanaged Code

---
+++ Firebrand.Demos.Demo25_Dynamic/Program.cs
@@ -0,0 +1,29 @@
+﻿using IronPython.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firebrand.Demos.Demo25_Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic d = "Hello";
+            Console.WriteLine(d);
+            d = 123;
+            Console.WriteLine(d);
+            d = new System.Dynamic.ExpandoObject();
+            d.Firstname = "Jipson";
+            d.Age = 33;
+            Console.WriteLine("{0} is {1} years old",d.Firstname,d.Age);
+            var pythonRuntime = Python.CreateRuntime();
+            dynamic pythonFile = pythonRuntime.UseFile("HelloPython.py");
+            pythonFile.SayHelloFromPython();
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Firebrand.Demos.Demo26_IDisposable/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firebrand.Demos.Demo26_IDisposable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal Sheerkhan = new Animal();
+            using (Human jenny = new Human(Sheerkhan))
+            {
+                jenny.Watch();
+                Sheerkhan.Run();
+                //jenny = null;
+            }
+            
+            
+            Sheerkhan.Run();
+            Console.ReadKey();
+        }
+    }
+    public class Human : IDisposable
+    {
+        private Animal _animal;
+        private bool isDisposed = false;
+        public Human(Animal animal)
+        {
+            // Constructor with no input is default constructor
+
+            _animal = animal;
+
+        }
+        public void UnWatch()
+        {
+            _animal.OnRun -= animal_OnRun;
+        }
+        public void Watch()
+        {
+            _animal.OnRun += animal_OnRun;
+        }
+
+        void animal_OnRun(object sender, EventArgs e)
+        {
+            Console.WriteLine("Scream like a girl and run for your life!!!");
+        }
+
+        protected virtual void Dispose(bool isDisposing)
+        {
+            if (!this.isDisposed)
+            {
+                if (isDisposing)
+                {
+                    // Release Manage resources...
+                    this.UnWatch();
+
+                }
+                // Release unmanaged resources
+                this.isDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        // Distructor to dispose the unmanaged code...
+        ~Human()
+        {
+            this.Dispose(false);
+        }
+    }
+    public class Animal
+    {
+        public event EventHandler OnRun;
+        public void Run()
+        {
+            Console.WriteLine("The animal is running after you!");
+            // If somebody listening then raise the event
+            if (OnRun != null)
+            {
+                OnRun(this, new EventArgs());
+            }
+
+        }
+    }
+}
